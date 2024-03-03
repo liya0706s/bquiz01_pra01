c699a7644c8e60cce3114d506310edbe55fe0801
Angie <liya0706s@gmail.com>
2024-03-03 22:12:03 +0800

    feat:step12-後台選單管理 撈的資料是主選單 欄位有計算次選單數 編輯次選單的按鈕是modal/submenu.php

--- back/menu.php
+++ back/menu.php
@@ -3,26 +3,30 @@
     <form method="post" action="./api/edit.php">
         <table width="100%">
             <tr class="yel">
-                <td width="45%">主選單名稱</td>
-                <td width="23%">選單連結網址</td>
-                <td width="23%">次選單數</td>
-                <td width="7%">顯示</td>
-                <td width="7%">刪除</td>
-                <td></td>
+                <td width="30%">主選單名稱</td>
+                <td width="30%">選單連結網址</td>
+                <td width="10%">次選單數</td>
+                <td width="10%">顯示</td>
+                <td width="10%">刪除</td>
+                <td width="10%"></td>
             </tr>
             <?php
-            $rows = $Menu->all();
+            $rows = $Menu->all(['menu_id' => 0]);
             foreach ($rows as $row) {
             ?>
-                <tr>
-                    <td width="45%"><img src="./img/<?= $row['img']; ?>" style="width:300px;height:30px"></td>
-                    <td width="23%"><input type="text" name="text[]" value="<?= $row['text']; ?>"></td>
-                    <!-- 判斷顯示狀態並讓checkbox狀態改變 -->
-                    <td width="7%"><input type="radio" name="sh" value="<?= $row['id']; ?>" <?= ($row['sh'] == 1) ? 'checked' : ''; ?>></td>
-                    <td width="7%"><input type="checkbox" name="del[]" value="<?= $row['id']; ?>"></td>
+                <tr style="text-align: center;">
+                    <td>
+                        <input type="text" name="text[]" value="<?= $row['text']; ?>">
+                    </td>
+                    <td><input type="text" name="href[]" value="<?= $row['href']; ?>"></td>
+                    <td>
+                        <?=$Menu->count(['menu_id'=>$row['id']]);?>
+                    </td>
+                    <td><input type="checkbox" name="sh[]" value="<?= $row['id']; ?>" <?= ($row['sh'] == 1) ? 'checked' : ''; ?>></td>
+                    <td><input type="checkbox" name="del[]" value="<?= $row['id']; ?>"></td>
                     <td>
                         <!-- 傳送表單和資料id的參數到api的upload.php, 才知道是哪個資料表哪筆資料要更新 -->
-                        <input type="button" onclick="op('#cover','#cvr','./modal/upload.php?table=<?= $do; ?>&id=<?= $row['id']; ?>')" value="更新圖片">
+                        <input type="button" onclick="op('#cover','#cvr','./modal/submenu.php?table=<?= $do; ?>&id=<?= $row['id']; ?>')">
                         <!-- 每筆資料都有一個對應的id, 後端才知道要編輯哪一筆 -->
                         <input type="hidden" name="id[]" value="<?= $row['id']; ?>">
                     </td>
@@ -33,7 +37,7 @@
             <tr>
                 <td width="200px">
                     <!-- 彈出視窗是由這個op()的js函式觸發的，使用網址參數來代替輸入的modal/檔名，並加上table參數 -->
-                    <input type="button" onclick="op('#cover','#cvr','./modal/<?= $do; ?>.php?table=<?= $do; ?>')" value="新增網站標題圖片">
+                    <input type="button" onclick="op('#cover','#cvr','./modal/<?= $do; ?>.php?table=<?= $do; ?>')" value="新增主選單">
                 </td>
 
                 <td class="cent">
