5cc3dfa0e14efd10f9566a5b77e3eef4ee7576e6
Angie <liya0706s@gmail.com>
2024-03-04 00:20:11 +0800

    feat:增加次選單的js

--- modal/submenu.php
+++ modal/submenu.php
@@ -3,7 +3,7 @@
 <h3>編輯次選單</h3>
 <hr>
 <form action="./api/submenu.php" method="post" enctype="multipart/form-data">
-    <table>
+    <table class="cent" id="sub">
         <tr>
             <td>次選單名稱</td>
             <td>次選單連結網址</td>
@@ -15,25 +15,38 @@
         ?>
             <tr>
                 <td>
-                    <input type="text" name="text[]" value="<?=$sub['text'];?>">
+                    <input type="text" name="text[]" value="<?= $sub['text']; ?>">
                 </td>
                 <td>
-                    <input type="text" name="href[]" value="<?=$sub['href'];?>">
+                    <input type="text" name="href[]" value="<?= $sub['href']; ?>">
                 </td>
                 <td>
-                    <input type="checkbox" name="del[]" value="<?=$sub['id'];?>">
+                    <input type="checkbox" name="del[]" value="<?= $sub['id']; ?>">
                 </td>
-                <input type="hidden" name="id[]" value="<?=$sub['id'];?>">
+                <input type="hidden" name="id[]" value="<?= $sub['id']; ?>">
             </tr>
         <?php } ?>
     </table>
-<div>
-    <input type="hidden" name="table" value="<?=$_GET['table'];?>">
-    <!-- 將主選單的id加入到隱藏欄位中，
+    <div>
+        <input type="hidden" name="table" value="<?= $_GET['table']; ?>">
+        <!-- 將主選單的id加入到隱藏欄位中，
     讓後臺的api知道這個表單的次選單資料是屬於哪個主選單的 -->
-    <input type="hidden" name="menu_id" value="<?=$_GET['id'];?>">
-    <input type="submit" value="修改確定">
-    <input type="reset" value="重置">
-    <input type="button" value="更多次選單" onclick="more()">
-</div>
+        <input type="hidden" name="menu_id" value="<?= $_GET['id']; ?>">
+        <input type="submit" value="修改確定">
+        <input type="reset" value="重置">
+        <input type="button" value="更多次選單" onclick="more()">
+    </div>
 </form>
+
+<script>
+    function more() {
+        // 宣告一個次選單的html字串
+        let item =
+        `<tr>
+            <td><input type="text" name="add_text[]" id=""></td>
+            <td><input type="text" name="add_href[]" id=""></td>
+        </tr>`
+        // 使用append()函式將html字串，添加到列表的最後面
+        $("#sub").append(item);
+    }
+</script>
