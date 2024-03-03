8db1526be8dca6b4d99f263231955ed785e70383
Angie <liya0706s@gmail.com>
2024-03-03 15:00:19 +0800

    feat:step6的./modal/upload.php

--- modal/m_upload.php
+++ modal/m_upload.php
@@ -1,8 +1,4 @@
 <?php
-// echo "$_POST";
-
-// 更新圖片按鈕會有js函式 op() GET傳值table和id
-
 switch ($_GET['table']) {
     case "title":
         echo "<h3>更新網站標題圖片</h3>";
@@ -15,28 +11,32 @@ switch ($_GET['table']) {
         break;
 }
 ?>
+
 <hr>
-<!-- 所有更新功能表單都會送到api中的update.php這支程式中處理 -->
 <form action="./api/update.php" method="post" enctype="multipart/form-data">
     <table>
         <tr>
-            <?php
-            switch ($_GET['table']) {
+        <?php
+            switch($_GET['table']) {
                 case "title":
                     echo "<td>標題區圖片</td>";
+                break;
                 case "mvim":
                     echo "<td>動畫圖片</td>";
+                break;
                 case "image":
                     echo "<td>校園映像圖片</td>";
+                break;
             }
-            ?>
-            <td><input type="file" name="img"></td>
+        ?>
+            <td><input type="file" name="img" id=""></td>
         </tr>
     </table>
-    <div class="ct">
-        <input type="hidden" name="table" value="<?=$_GET['table'];?>">
-        <input type="hidden" name="id" value="<?=$_GET['id'];?>">
-        <input type="submit" value="更新">
+    <div>
+        <input type="hidden" name="table" value="<?= $_GET['table']; ?>">
+        <input type="hidden" name="id" value="<?= $_GET['id']; ?>">
+        <!-- 哪個資料表的圖片 -->
+        <input type="submit" value="新增">
         <input type="reset" value="重置">
     </div>
 </form>
