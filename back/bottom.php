5d79cc8b4e1fd61e744e73fabc73f3a8ee1e0a8d
liya0706s <liya0706s@gmail.com>
2024-03-04 13:53:43 +0800

    feat:step16-製作後台訪客計數器及頁尾版權文字管理

---
+++ back/bottom.php
@@ -0,0 +1,26 @@
+<div style="width:99%; height:87%; margin:auto; overflow:auto; border:#666 1px solid;">
+    <p class="t cent botli">頁尾版權資料管理</p>
+    <form method="post" action="./api/edit_info.php">
+        <table width="100%">
+            <tr class="yel">
+                <td width="50%">頁尾版權資料:</td>
+                <td width="50%">
+                    <input type="text" name="bottom" value="<?= $Bottom->find(1)['bottom']; ?>">
+                    <input type="hidden" name="table" value=<?= $do; ?>>
+                </td>
+            </tr>
+        </table>
+
+        <table style="margin-top:40px; width:70%;">
+            <tr>
+                <td width="200px;"></td>
+                <td class="cent">
+                    <!-- 為了後端方便操作不同的功能，隱藏一個資料表的變數在這 -->
+                    <input type="hidden" name="table" value="<?= $do; ?>">
+                    <input type="submit" value="修改確定">
+                    <input type="reset" value="重置">
+                </td>
+            </tr>
+        </table>
+    </form>
+</div>
